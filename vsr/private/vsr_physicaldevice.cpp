89ac3b7da12fc3301ec1d3a3d25140ec23e31c05
linyuan <sanglin0307@126.com>
2018-03-29 17:18:16 +0800

    update

--- vsr/private/vsr_physicaldevice.cpp
+++ vsr/private/vsr_physicaldevice.cpp
@@ -13,6 +13,46 @@ VkPhysicalDevice_T* VkPhysicalDevice_T::Get()
 	return _instance;
 }
 
+void VkPhysicalDevice_T::GetPhysicalDeviceQueueFamilyProperties(
+	uint32_t*                                   pQueueFamilyPropertyCount,
+	VkQueueFamilyProperties*                    pQueueFamilyProperties)
+{
+	// use all in one first!
+	*pQueueFamilyPropertyCount = 1;
+	if (pQueueFamilyProperties == nullptr)
+		return;
+
+	pQueueFamilyProperties[0].queueFlags = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT;
+	pQueueFamilyProperties[0].queueCount = 1;
+	pQueueFamilyProperties[0].minImageTransferGranularity = { 1,1,1 };
+	pQueueFamilyProperties[0].timestampValidBits = 64;
+}
+
+void VkPhysicalDevice_T::GetPhysicalDeviceFeatures(
+	VkPhysicalDeviceFeatures*                   pFeatures)
+{
+	*pFeatures = Features;
+}
+
+void VkPhysicalDevice_T::GetPhysicalDeviceProperties(
+	VkPhysicalDeviceProperties*                 pProperties)
+{
+
+}
+
+void VkPhysicalDevice_T::GetPhysicalDeviceFormatProperties(
+	VkFormat                                    format,
+	VkFormatProperties*                         pFormatProperties)
+{
+
+}
+
+void VkPhysicalDevice_T::GetPhysicalDeviceMemoryProperties(
+	VkPhysicalDeviceMemoryProperties*           pMemoryProperties)
+{
+
+}
+
 VKAPI_ATTR VkResult VKAPI_CALL vkEnumeratePhysicalDevices(
 	VkInstance                                  instance,
 	uint32_t*                                   pPhysicalDeviceCount,
@@ -31,14 +71,14 @@ VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceQueueFamilyProperties(
 	uint32_t*                                   pQueueFamilyPropertyCount,
 	VkQueueFamilyProperties*                    pQueueFamilyProperties)
 {
-
+	return physicalDevice->GetPhysicalDeviceQueueFamilyProperties(pQueueFamilyPropertyCount, pQueueFamilyProperties);
 }
 
 VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceFeatures(
 	VkPhysicalDevice                            physicalDevice,
 	VkPhysicalDeviceFeatures*                   pFeatures)
 {
-
+	return physicalDevice->GetPhysicalDeviceFeatures(pFeatures);
 }
 
 VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceFormatProperties(
@@ -46,19 +86,19 @@ VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceFormatProperties(
 	VkFormat                                    format,
 	VkFormatProperties*                         pFormatProperties)
 {
-
+	return physicalDevice->GetPhysicalDeviceFormatProperties(format, pFormatProperties);
 }
 
 VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceProperties(
 	VkPhysicalDevice                            physicalDevice,
 	VkPhysicalDeviceProperties*                 pProperties)
 {
-
+	return physicalDevice->GetPhysicalDeviceProperties(pProperties);
 }
 
 VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceMemoryProperties(
 	VkPhysicalDevice                            physicalDevice,
 	VkPhysicalDeviceMemoryProperties*           pMemoryProperties)
 {
-
+	return physicalDevice->GetPhysicalDeviceMemoryProperties(pMemoryProperties);
 }
