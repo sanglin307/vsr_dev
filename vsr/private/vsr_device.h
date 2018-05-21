21fe9a3cbdc003c51092702bc9445acc49016277
sanglin307 <sanglin0307@126.com>
2018-05-21 23:24:27 +0800

    update

--- vsr/private/vsr_device.h
+++ vsr/private/vsr_device.h
@@ -18,5 +18,32 @@ struct VkDevice_T : public MemoryAlloc<VkDevice_T, VK_SYSTEM_ALLOCATION_SCOPE_DE
 	std::vector<VkQueue>                       _vecQueues;
 	VkPhysicalDevice                           _physicalDevice;
 
-	std::list<vsrDeviceResource*>               _listResource;
+	std::list<vsrDeviceResource*>              _listResource;
+};
+
+struct vsrStage {
+	vsrStage();
+	virtual ~vsrStage();
+	virtual void Process() = 0;
+	std::thread   *_pthread;
+	std::atomic_bool _running;
+};
+
+struct vsrStage_InputAssemble : public vsrStage {
+	vsrStage_InputAssemble();
+	~vsrStage_InputAssemble();
+	virtual void Process();
+};
+
+struct vsrStage_VertexShader : public vsrStage {
+	vsrStage_VertexShader();
+	virtual void Process();
+};
+
+
+struct vsrProcessor {
+	vsrProcessor();
+	~vsrProcessor();
+
+	std::vector<vsrStage*>   _stages;
 };
